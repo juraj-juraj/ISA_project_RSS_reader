3c4dc3430577d2bc22b43db5877ac5555a95a2af
juraj-juraj <juraj.novosad.sk@gmail.com>
2022-10-13 19:20:14 +0200

    Tidy main and error logs

--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,29 @@
 #include "constants.h"
 #include <iostream>
 #include <fstream>
+#include <memory.h>
+
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <netdb.h>
 
 #include "parameter_parser/parameterparser.h"
 #include "utils/exceptions.h"
 #include "utils/logger.h"
+#include "include/urlParser/urlparser.h"
 
 int main(int argc, const char *argv[])
 {
+    auto logger = std::make_shared<Utils::logger>(std::cout, std::cerr);
     ParameterParser::ParameterParser parser;
-    parser.addValueParameter(FEEDFILE_ARG);
+    parser.addValueParameter(FEEDFILE_ARG).isFile();
     parser.addParameter(AUTHOR_ARG);
     parser.addParameter(DATESHOW_ARG);
-
-    Utils::logger logger(std::cout, std::cerr);
+    parser.addParameter(URLSHOW_ARG);
+    parser.addPositionalParameter(URLPOS_ARG, 0);
+    parser.addValueParameter(CERTFILE_ARG).isFile();
+    parser.addValueParameter(CARTADDR_ARG).isDirectory();
+    parser.addParameter(HELP_ARG);
 
     try
     {
@@ -22,19 +32,27 @@ int main(int argc, const char *argv[])
 
         for(const auto& [key, value] : parsedValues)
         {
-            logger.write("something out");
+            logger->write("something out");
             std::cout << "key: " << key << std::endl;
         }
+
+
     }
+    catch(const feedreaderException::URLParsing &err)
+    {
+        logger ->errWrite("Error parsing url addresses: %s", err.what());
+    }
+
     catch(const feedreaderException::argumentParsing& err)
     {
-        std::cout << "Error parsing arguments: " << err.what() << std::endl;
+        logger->errWrite("Error parsing arguments: %s\n", err.what());
     }
     catch(...)
     {
+        logger->errWrite("Unknown error raised \n");
         std::cout << "Unknown error raised" << std::endl;
     }
 
-    std::cout << "hello world" << std::endl;
+
     return 0;
 }
